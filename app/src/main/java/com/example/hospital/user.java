2063461f06a749dff5949898ecd37303c77ce4ea
sa2961999 <samarbasiouni55@gmail.com>
2022-05-06 23:16:26 +0200

    register activity and login design

---
+++ app/src/main/java/com/example/hospital/user.java
@@ -0,0 +1,19 @@
+package com.example.hospital;
+
+public class user {
+    public String firstname;
+    public String lastname;
+     public String addess;
+     public String mobile;
+
+    public user() {
+    }
+
+    public user(String firstname, String lastname, String addess, String mobile) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.addess = addess;
+        this.mobile = mobile;
+    }
+}
+
